0886e9e36f2f6b35701947f0363f9be308423f38
Abenezer <abenezermamo720@gmail.com>
2025-09-11 23:25:12 -0400

    program setup

--- program.c
+++ program.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stddef.h>
 #include <time.h>
 
-struct tm;
- 
+	time_t payload;
+	// = NULL;
+	//time_t session;
+	struct tm session;
+
 int main(void)
 {
+
 //    struct time_t start = {.tm_year=2025-1900, .tm_mday=1};
     //mktime(&start);
 
 //	printf("%p", &tm);
 	
-	const struct time_t ** *payload;
-	//mktime(&payload);
-	const struct tm *session;
-	long int wait;
+	//struct time_t payload;
+	//mktime(&session);
+//	const struct tm *session;
+//	const struct payload *current;
 	
-	wait = sizeof(session);
-	printf("Session: %s\n", &session);	
-	printf("Current: %s\n", wait);	
+//	long int wait;
+	
+//	wait = sizeof(session);
+//	printf("Session: %s\n", localtime(&payload));
+//	printf("Value: %d\n", (int)*current);
+	printf("time: %s\n", ctime((const long int *)localtime(&payload)));
+	printf("mktime: %ld\n", mktime(&session));
+	
+	//printf("Current: %s\n", wait);	
 //	printf("%s \n", mktime(&start);
 //	printf("%s\n", &payload);	
 //	printf("%d", time(&payload));	
