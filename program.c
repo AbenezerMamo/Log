5f8753a3d94bfb2286c89a0367cca22de1f45946
Abenezer <abenezermamo720@gmail.com>
2025-09-12 06:58:56 -0400

    msg

--- program.c
+++ program.c
@@ -4,36 +4,54 @@
 #include <stddef.h>
 #include <time.h>
 
-	time_t payload;
-	// = NULL;
-	//time_t session;
-	struct tm session;
-
+//time_t payload = time();
 int main(void)
 {
 
-//    struct time_t start = {.tm_year=2025-1900, .tm_mday=1};
-    //mktime(&start);
+int days[31];
+time_t time(time_t *timer);
+time_t now;
 
-//	printf("%p", &tm);
+
+	
+	for (int i = 0; i < 3; i++)
+	{
+		now = time(NULL);
+		time_t * session;
+		struct tm *localTimeInfo;
+		localTimeInfo = localtime(&now);
+		char message[10];
+		printf("Local: %s", asctime(localTimeInfo));	
+		printf("Msg %d: ", i);
+		scanf("%s", message);
+	
+//		printf("Sent!\n"); 
+	
+		days[i] = *(message);
+	}
+
+	printf("\n");
+
+for (int x = 0; x < 3; x++)
+{
+	
+	printf("Msg %d: %c\n", x, days[x]);
+}
+	//printf("Seconds since Epoch: %ld \n", session);
+	//struct tm start = {.tm_year=2025-1900, .tm_mday=1};
+	//mktime(&start);
+
+//	printf("Start: %s\n", &start);
 	
 	//struct time_t payload;
 	//mktime(&session);
 //	const struct tm *session;
 //	const struct payload *current;
 	
-//	long int wait;
+	long int wait;
 	
 //	wait = sizeof(session);
-//	printf("Session: %s\n", localtime(&payload));
-//	printf("Value: %d\n", (int)*current);
-	printf("time: %s\n", ctime((const long int *)localtime(&payload)));
-	printf("mktime: %ld\n", mktime(&session));
-	
-	//printf("Current: %s\n", wait);	
-//	printf("%s \n", mktime(&start);
-//	printf("%s\n", &payload);	
-//	printf("%d", time(&payload));	
-//	printf("%s", time_t(wait));	
-return 1;
+	//printf("Session: %s\n", ctime(&localTimeInfo));
+	//printf("sizeof: %s\n", asctime(&now));
+return 0;
 }
